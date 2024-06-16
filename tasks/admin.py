9b6f4c47bab8218cc656dc24580d89e808a0d752
evahaverdova <haverdovaeva@gmail.com>
2024-06-16 14:52:46 +0200

    Backup and changes for admin setup

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task, TaskStatus
 
-# Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'completed', 'start_time', 'end_time', 'user', 'category')  # Prispôsobené podľa modelu
+    search_fields = ('title', 'description')
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(TaskStatus)
